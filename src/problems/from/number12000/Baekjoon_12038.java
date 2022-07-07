2fe032796e4ca13a38dd40981e7a29e9acd46e44
Joon <shuangjutou@naver.com>
2022-07-07 22:15:19 +0900

    2022-07-07-02, [BOJ 12038]

--- src/problems/from/number12000/Baekjoon_12038.java
+++ src/problems/from/number12000/Baekjoon_12038.java
@@ -7,13 +7,11 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Baekjoon_12038ING {
+public class Baekjoon_12038 {
 	static InputStreamReader isr = new InputStreamReader(System.in);
 	static BufferedReader br = new BufferedReader(isr);
 	static ArrayList<String> candidatesList = new ArrayList<>();
 	static int goal;
-	static BigInteger cCount;
-	static BigInteger vCount;
 
 	public static int toInt(String s) {
 		return Integer.parseInt(s);
@@ -28,39 +26,60 @@ public class Baekjoon_12038ING {
 			int nextLength = candidate.length() + 1;
 			if (consonantGettable && nextLength < goal) {
 				getCandidate(candidate + "c", false);
-				cCount.add(new BigInteger("1"));
+				
 			}
 			getCandidate(candidate + "v", true);
-			vCount.add(new BigInteger("1"));
 		}
 	}
 	
-	public static BigInteger answer() throws IOException {
+	public static BigInteger[] answer() throws IOException {
 		int cases = toInt(br.readLine());
+		BigInteger[] result = new BigInteger[cases];
 		StringTokenizer st;
 		for (int i=0; i<cases; i++) {
+			result[i] = new BigInteger("0");
 			candidatesList = new ArrayList<>();
-			cCount = new BigInteger("0");
-			vCount = new BigInteger("0");
 			st = new StringTokenizer(br.readLine());
 			int C = toInt(st.nextToken());
 			int V = toInt(st.nextToken());
 			int L = toInt(st.nextToken());
 			goal = L;
 			getCandidate("", true);
-			System.out.println(cCount + " " + vCount);
-			for (String s : candidatesList) {
-				System.out.println(s);
+			for (String line : candidatesList) {
+				int cCount = 0;
+				int vCount = 0;
+				for (char c : line.toCharArray()) {
+					if (c == 'c') {
+						cCount++;
+					} else {
+						vCount++;
+					}
+				}
+				BigInteger cCases = new BigInteger(toString(C)).pow(cCount);
+				BigInteger vCases = new BigInteger(toString(V)).pow(vCount);
+				result[i] = result[i].add(cCases.multiply(vCases));	
 			}
+			
 		}
-		
-		
-		
-		return new BigInteger("100");
+		return result;
 	}
 	public static void main(String args[]) throws IOException {
 		int a = (int) Math.pow(10, 9)+7;
-		System.out.println(answer().mod(new BigInteger(Integer.toString(a))));
+		BigInteger[] result = answer();
 		
+		for (int i=0; i<result.length; i++) {
+			
+			System.out.printf("Case #%d: %d\n",i+1, result[i].mod(new BigInteger(Integer.toString(a))).intValue());
+		}
 	}
 }
+
+/*
+3
+1 1 4
+1 2 2
+50 50 15
+
+1
+1 1 3
+*/
