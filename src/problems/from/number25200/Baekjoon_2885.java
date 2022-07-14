db5383ac91f367c71e61fb0f74f07d4bc3320b15
JunInCompany <shuangjutou@naver.com>
2022-07-14 11:55:33 +0900

    2022-07-14-01, [BOJ 2885]

---
+++ src/problems/from/number25200/Baekjoon_2885.java
@@ -0,0 +1,38 @@
+package problems.from.number25200;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_2885 {
+	
+	public static int toInt(String s) {
+		return Integer.parseInt(s);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		int K = toInt(br.readLine());
+		int chocolateSize = 1;
+		
+		while (K > chocolateSize) {
+			chocolateSize *= 2;
+		}
+		System.out.print(chocolateSize + " ");
+		int now = K;
+		int sliceCount = 0;
+		
+		while (now > 0) {
+			if (chocolateSize > now) {
+				chocolateSize /= 2;
+				sliceCount++;
+			} else {
+				now -= chocolateSize;
+			}
+		}
+		System.out.println(sliceCount);
+	}
+
+}
