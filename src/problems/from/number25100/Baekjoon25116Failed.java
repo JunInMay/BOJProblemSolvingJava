700e76cd5573000bfe592a86fe81c016f07adabe
Joon <shuangjutou@naver.com>
2023-02-09 19:38:23 +0900

    BOJ 25116 finally failed....

--- src/problems/from/number25100/Baekjoon25116Failed.java
+++ src/problems/from/number25100/Baekjoon25116Failed.java
@@ -3,11 +3,15 @@ package problems.from.number25100;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
-public class Baekjoon25116 {
+public class Baekjoon25116Failed {
 
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
@@ -24,7 +28,6 @@ public class Baekjoon25116 {
 		while (minLevel <= maxLevel) {
 			long midLevel = (minLevel + maxLevel) / 2;
 			long now = midLevel*(midLevel+1)/2;
-			System.out.println("now : " + now + " midL : " + midLevel);
 			if (now < requiredStageCount) {
 				minLevel = midLevel+1;
 			} else {
@@ -33,11 +36,11 @@ public class Baekjoon25116 {
 		}
 		requiredLevel = maxLevel;
 		long leastLevel = requiredLevel-1;
-		System.out.println("requiredLevel : " + requiredLevel);
 		// 1 = 0
 		// 2, 3 = 1
 		// 4, 5, 6 = 2
 		// 7, 8, 9, 10 = 3 ...
+		
 		
 		// 부분합 구하기
 		long[] stageDifficulties = new long[stageCount+1];
@@ -51,26 +54,72 @@ public class Baekjoon25116 {
 		ArrayList<Long> leastSubtotalParts = new ArrayList<>();
 		// 몇개씩 묶이는지?
 		int stageGroupMemberCount = (int)(stageCount - leastLevel);
-		System.out.println("몇개씪 묶이나 ? : " + stageGroupMemberCount);
 		// leastLevel 의 부분합 중 최소값 을 구하고 requiredStageDifficulty를 만족시키는 가수를 찾기
 		for (int i=1; i<=requiredLevel; i++) {
 			leastSubtotalParts.add(subtotals[i+stageGroupMemberCount-1]-subtotals[i-1]);
 		}
 		
-		long result = (long) Math.ceil((double)(Math.max(0, requiredDifficulty - Collections.min(leastSubtotalParts)))/stageGroupMemberCount);
-		System.out.println("최소 : " + result);
+		long leastMinValue = requiredDifficulty;
+		if (leastSubtotalParts.size() > 0) {
+			leastMinValue = Collections.min(leastSubtotalParts);
+		}
+		long result = (long) Math.ceil((double)(Math.max(0, requiredDifficulty - leastMinValue))/stageGroupMemberCount);
+		BigDecimal bd1 = new BigDecimal(0)
+				.max(new BigDecimal(requiredDifficulty - leastMinValue))
+				.divide(new BigDecimal(stageGroupMemberCount), MathContext.DECIMAL128)
+				.setScale(0, RoundingMode.CEILING);
 		
 		ArrayList<Long> mainSubtotalParts = new ArrayList<>();
 		stageGroupMemberCount = (int)(stageCount - requiredLevel);
 		for (int i=1; i<=requiredLevel+1; i++) {
 			mainSubtotalParts.add(subtotals[i+stageGroupMemberCount-1]-subtotals[i-1]);
 		}
-		System.out.println(mainSubtotalParts);
-	}
+		Collections.sort(mainSubtotalParts, Comparator.reverseOrder());
+		
+		int mainCount = (int)(requiredStageCount - (requiredLevel*(requiredLevel+1)/2));
+		
+		long minValue = Long.MAX_VALUE;
+		for (int i=0; i<mainCount; i++) {
+			minValue = Math.min(minValue, mainSubtotalParts.get(i));
+		}
+		
+		BigDecimal bd2 = new BigDecimal(0)
+				.max(new BigDecimal(requiredDifficulty - minValue))
+				.divide(new BigDecimal(stageGroupMemberCount), MathContext.DECIMAL128)
+				.max(bd1)
+				.setScale(0, RoundingMode.CEILING);
+		
+		result = Math.max(result, (long) Math.ceil((double)(Math.max(0, requiredDifficulty - minValue))/stageGroupMemberCount));
+		System.out.println(bd2);
+	}	
 
 }
 
 /*
+2 1000000000000000000 3
+1 0
+2 1000000000000000000 2
+1 0
+
+3 1000000000000000000 3
+1 2 0
+
+4 10 1
+0 0 0 0
+
+4 12 10
+1 2 3 4
+
+3 10 6
+0 0 0
+
+3 10 1
+0 0 0
+
+5 12 1
+1 2 3 4 5
+
+
 3 17 2
 3 4 5
 
@@ -86,5 +135,6 @@ public class Baekjoon25116 {
 4 1 2
 
 100000 1 5000050000
+1 1234
 
 */
