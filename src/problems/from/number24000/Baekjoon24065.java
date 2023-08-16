71e82439a8503472e4980f09ea3678c977e4f89c
Chana <twszak01@gmail.com>
2023-08-17 04:05:09 +0900

    BOJ 24065 코드 최종 수정 및 주석

--- src/problems/from/number24000/Baekjoon24065.java
+++ src/problems/from/number24000/Baekjoon24065.java
@@ -16,13 +16,17 @@ public class Baekjoon24065 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int pointCount = Integer.parseInt(st.nextToken());
 			int leastSize = Integer.parseInt(st.nextToken());
-			double leastRoot = Math.sqrt(leastSize);
 
 			long result = 0;
 			for (int i = 1; i <= pointCount - 1; i++) {
 				for (int j = 0; j <= pointCount - i - 1; j++) {
-					double root = Math.sqrt(i * i + j * j);
-					if (root >= leastRoot) {
+
+					int size = i * i + j * j; // i, j 각각의 점으로부터 그려지는 정사각형의 크기
+					if (size >= leastSize) {
+						/*
+						 * 정사각형이 되는 점 두개 i, j를 구했다면, 그 정사각형이 차지하는 너비는 i + j이다.
+						 * 가로, 세로에 각각 N - (i + j) 만큼 정사각형을 그릴 수 있다.
+						 */
 						result += (pointCount - (i + j)) * (pointCount - (i + j));
 					}
 				}
